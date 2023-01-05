78eae51b1910162b96a869162049d9c2fc06fc37
Tharlis David - 747 <tharlis.pereira@cairunet.ad.br>
2023-01-05 18:50:09 -0400

    Alterar acentuação Algoritimo_2

--- src/Algoritimo_2/Main.java
+++ src/Algoritimo_2/Main.java
@@ -6,21 +6,21 @@ public class Main {
 	public static void main(String [] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Digite o primeiro numero inteiro!");
+		System.out.println("Digite o primeiro número inteiro!");
 		Integer num1 = scanner.nextInt();
 
-		System.out.println("Digite o segundo numero inteiro!");
+		System.out.println("Digite o segundo número inteiro!");
 		Integer num2 = scanner.nextInt();
 
-		System.out.println("Digite o terceiro numero inteiro!");
+		System.out.println("Digite o terceiro número inteiro!");
 		Integer num3 = scanner.nextInt();
 
 		if (num1>num2) {
-			System.out.println("Legal " + num1 + " foi o maior numero que voce digitou!");
+			System.out.println("Legal, " + num1 + " foi o maior número que voce digitou!");
 		}else if (num2>num3) {
-			System.out.println("Legal " + num2 + " foi o maior numero que voce digitou!");
+			System.out.println("Legal, " + num2 + " foi o maior número que voce digitou!");
 		}else {
-			System.out.println("Legal " + num3 + " foi o maior numero que voce digitou!");
+			System.out.println("Legal " + num3 + " foi o maior número que voce digitou!");
 		}			
 
 
